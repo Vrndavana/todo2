9ff613f98614d53145e0298fd100cbd67f41178e
Vrndavana <Vrmdavama.th@gmail.com>
2020-03-16 13:00:42 -0700

    Todoing

---
+++ todo2/src/Components/TodoForm.js
@@ -0,0 +1,51 @@
+import React, {useState, useReducer} from 'react'
+import {initialState, reducerTodo} from '../Reducers/reducerTodo'
+import Todos from '../Components/Todo'
+
+
+
+const TodoForm = () => {
+
+    const [state, dispatch] = useReducer(reducerTodo, initialState)
+    const [newTodo, setNewTodo] = useState("")
+    const handleChanges = e => {
+        setNewTodo(e.target.value)
+    }
+
+    const handleSubmit = e => {
+        e.preventDefault();
+        dispatch({type: 'ADD_TODO', payload: newTodo})
+        console.log(state)
+    }
+    
+    const handleClear = e => {
+        e.preventDefault();
+        dispatch({type: "CLEAR", payload: newTodo});
+    }
+    
+        return (
+            <div>
+                <form>
+                    <input
+                    type="text"
+                    placeholder="Add Todo"
+                    name="newTodo"
+                    id='newTodo'
+                    value={newTodo}
+                    onChange={handleChanges}></input>
+                    <button
+                    type="submit"
+                    onClick={handleSubmit}>Add</button>
+                    <button
+                    type="submit"
+                    onClick={handleClear}>Clear Completed</button>
+                </form>
+             <Todos 
+             state={state}
+             dispatch={dispatch}
+             id={state.id} 
+             newTodo={newTodo}/>
+            </div>
+        )
+}
+export default TodoForm
